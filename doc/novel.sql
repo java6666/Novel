7f390f449b11018d520647dd400de650befb918e
韩可斌 <838195225@qq.com>
2017-04-06 16:30:23 +0800

    更新信息

--- doc/novel.sql
+++ doc/novel.sql
@@ -1,8 +1,9 @@
 /*
 SQLyog Ultimate v10.00 Beta1
-MySQL - 5.5.54 : Database - noval
+MySQL - 5.5.54 : Database - novel
 *********************************************************************
-*/
+*/
+
 
 /*!40101 SET NAMES utf8 */;
 
@@ -12,9 +13,9 @@ MySQL - 5.5.54 : Database - noval
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`noval` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`novel` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
 
-USE `noval`;
+USE `novel`;
 
 /*Table structure for table `novel_category` */
 
