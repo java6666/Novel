dc7003821fe49e444cd24c6066f9e2cd9625ae3a
于东平 <648114574qq.com>
2017-04-11 10:07:55 +0800

    更新实体类

--- src/main/java/model/entity/other/EmailBox.java
+++ src/main/java/model/entity/other/EmailBox.java
@@ -14,4 +14,44 @@ public class EmailBox {
     private Integer recipientsId; //收件人id
     private String content;        //邮件内容
     private Date createDate;       //发件日期
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getAddresseeId() {
+        return addresseeId;
+    }
+
+    public void setAddresseeId(Integer addresseeId) {
+        this.addresseeId = addresseeId;
+    }
+
+    public Integer getRecipientsId() {
+        return recipientsId;
+    }
+
+    public void setRecipientsId(Integer recipientsId) {
+        this.recipientsId = recipientsId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
 }
