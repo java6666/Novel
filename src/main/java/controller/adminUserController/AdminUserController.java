b4a804e626aa2d4d8a2fb087a8f46bfc222da017
闫哲 <1379414939@qq.com>
2017-04-12 22:32:08 +0800

    AdminUserController服务添加、processingApplication.jsp页面、AuthorDao及其sql

--- src/main/java/controller/adminUserController/AdminUserController.java
+++ src/main/java/controller/adminUserController/AdminUserController.java
@@ -23,6 +23,7 @@ public class AdminUserController {
     //作者详细信息dao
     @Resource
     private AuthorDao authorDao;
+
     //查看所有用户
     @RequestMapping(value = "/admin/showUsers",method = RequestMethod.GET)
     public String showUsers(Model model){
@@ -30,6 +31,7 @@ public class AdminUserController {
         model.addAttribute("userEntities",userEntities);
         return "/WEB-INF/admin/showUsers.jsp";
     }
+
     //查看用户详细信息
     @RequestMapping(value = "/admin/showUserDetails",method = RequestMethod.GET,produces = "text/html;charset=UTF-8")
     public String showUserDetails(Model model,Integer id,Integer userType) {
@@ -48,4 +50,18 @@ public class AdminUserController {
          model.addAttribute("userType",id);
         return "/WEB-INF/admin/showUsers.jsp";
     }
+
+    //接收到消息后根据id通过请求操作
+    @RequestMapping(value = "/admin/applicationApproved",method = RequestMethod.GET)
+    public String applicationApproved(Model model,Integer id){
+        userEntityDao.updateUserEntityByUserId(id,2);
+        return null;
+    }
+    //接收到消息后根据id驳回请求操作并删除Author中相关信息
+    @RequestMapping(value = "/admin/rejectTheRequest",method = RequestMethod.GET)
+    public String rejectTheRequest(Model model,Integer id){
+        authorDao.deleteAuthorInfoByUserId(id);
+        return null;
+    }
+
 }
