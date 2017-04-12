a93b3aebb940fe98d17d73f7628cedfab8d0a3bf
闫哲 <1379414939@qq.com>
2017-04-12 21:36:48 +0800

    添加dao层及xml中sql方法（根据id修改用户类型）

--- src/main/java/model/dao/UserEntityDao.java
+++ src/main/java/model/dao/UserEntityDao.java
@@ -11,6 +11,10 @@ public interface UserEntityDao {
     UserEntity selectUserByUser(UserEntity userEntity);
     UserEntity selectUserById(int id);
     boolean updateUserByUser(UserEntity userEntity);
+
     //查询novel_user表中所有用户信息
     List<UserEntity> selectUserEntityAll();
+
+    //根据id修改用户类型(userType为修改成的用户类型)
+    void updateUserEntityByUserId(Integer id,Integer userType);
 }
