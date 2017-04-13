df76ed1d75a8313e89bea9f64a0a811558fc29e2
闫哲 <1379414939@qq.com>
2017-04-13 20:26:38 +0800

    添加adminservice

--- src/main/java/model/service/admin/DoRequest.java
+++ src/main/java/model/service/admin/DoRequest.java
@@ -1,7 +1,10 @@
 package model.service.admin;
 
+import org.springframework.transaction.annotation.Transactional;
+
 /**
  * Created by yz on 2017/4/13.
  */
+@Transactional
 public class DoRequest {
 }
