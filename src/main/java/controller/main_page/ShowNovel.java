d12daeba43320982edbccfba5acf7c6b2cd47691
李杰 <1585885237@qq.com>
2017-04-24 19:12:01 +0800

    xx

--- src/main/java/controller/main_page/ShowNovel.java
+++ src/main/java/controller/main_page/ShowNovel.java
@@ -1,6 +1,11 @@
 package controller.main_page;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import model.dao.NovelCommentDao;
 import model.dao.NovelEntityDao;
+import model.entity.novel.NovelComment;
+import model.entity.novel.NovelEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,10 +22,19 @@ import java.util.List;
 public class ShowNovel {
     @Resource
     private NovelEntityDao novelEntityDao;
+
+    @Resource
+    private NovelCommentDao novelCommentDao;
     @RequestMapping(path = "/showNovel",method = RequestMethod.POST,produces = "application/json;charset=utf-8")
     @ResponseBody
-    public List<Object> showNovel(String currentPage,Integer novelId){
+    public List<Object> showNovel(Integer currentPage,Integer novelId){
         ArrayList<Object> objects = new ArrayList<>();
+        PageHelper.startPage(currentPage,3);
+        NovelEntity novelEntity = novelEntityDao.novelLatestChapterById(novelId);
+
+        List<NovelComment> novelComments = novelCommentDao.selectByNovelId(novelId);
+        PageInfo<NovelComment> novelCommentPageInfo = new PageInfo<>(novelComments);
+        PageInfo<Object> pageInfo = new PageInfo<>();
         return objects;
     }
 }
--- src/main/java/model/dao/NovelCommentDao.java
+++ src/main/java/model/dao/NovelCommentDao.java
@@ -1,10 +1,14 @@
 package model.dao;
 
+import model.entity.novel.NovelComment;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created by LJ on 2017/4/11.
  */
 
 public interface NovelCommentDao {
+    List<NovelComment> selectByNovelId(Integer novelId);
 }
