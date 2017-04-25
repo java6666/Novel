25ac71c591de7d9a4f57558b0fb38cc55d04e56a
ZhaoFuXiang <1919037618@qq.com>
2017-04-25 14:18:41 +0800

    赵福祥 更新

--- src/main/java/model/dao/NovelEntityDao.java
+++ src/main/java/model/dao/NovelEntityDao.java
@@ -28,4 +28,7 @@ public interface NovelEntityDao {
     List<NovelEntity> selectNovelByClick();//查找点击量高的书籍ydp
 
     List<NovelEntity> selectNovelByWords(@Param("words")String word);//通过关键字搜索小说ydp
+
+    List<NovelEntity> selectNovelByBuyCount();
+    List<NovelEntity> selectNovelClickRate();
 }
